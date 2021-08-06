8ab3f14f2770eaaa2c0c37f109c18fcb25b6c5c3
joaldotavares <joaldo.junior@ucsal.edu.br>
2021-08-06 13:52:24 -0300

    Refatoração do if

--- transacao/src/main/java/br/com/zup/transacao/controller/TransacaoController.java
+++ transacao/src/main/java/br/com/zup/transacao/controller/TransacaoController.java
@@ -1,17 +1,17 @@
 package br.com.zup.transacao.controller;
 
-import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.zup.transacao.dto.TransacaoDTO;
 import br.com.zup.transacao.model.Cartao;
@@ -38,16 +38,13 @@ public class TransacaoController {
 	@GetMapping("/cartao/{id}")
 	public ResponseEntity<Page<?>> exibir(@PathVariable String id,
 			@PageableDefault(sort = "efetivadaEm", page = 0, size = 10) Pageable paginacao) {
-		
-		Optional<Cartao> cartao = cartaoRepository.findById(id);
+
+		Cartao cartao = cartaoRepository.findById(id).orElseThrow(
+				() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cartao com id \"" + id + "\" não existe."));
 		Page<Transacao> compras = transacaoRepository.findByCartaoId(id, paginacao);
 
-		if (cartao.isEmpty()) {
-			logger.error("Cartao com id \"" + id + "\" não existe.");
-			return ResponseEntity.notFound().build();
-		}
 		logger.info("Cartão encontrado com os seguintes dados, " + cartao.toString());
-		
+
 		return ResponseEntity.ok().body(TransacaoDTO.converter(compras));
 	}
 }
